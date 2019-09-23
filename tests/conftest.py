6a9726fb0bbd799826baf564df151b34898facbc
Fachrul Ch <fachrul.fch@gmail.com>
2019-09-23 10:52:53 +0700

    put console log in setup and teardown

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,14 @@ from selene import factory
 from testlib.ui import driver, application
 
 
-@pytest.fixture(scope='session')
+# @pytest.fixture(scope='session')
+@pytest.fixture
 def app():
+	print("\n==> Setup up App driver")
 	mobile_driver = driver.get()
 	factory.set_shared_driver(mobile_driver)
 
 	yield application
 
+	print("\n==> Closing down App driver")
 	driver.close(mobile_driver)
--- tests/scenarios/instagram_test.py
+++ tests/scenarios/instagram_test.py
@@ -1,6 +1,20 @@
 from selene.conditions import visible
-
-import configs
+from testlib.ui.screens import home_screen
+from time import sleep
 
 def test_pertamax(app):
+	print('Runnning first test')
+	home_screen.btn_explore.click()
+	home_screen.btn_home.click()
+	sleep(3)
+	print('Calling general method')
+	app.mondar_mandir()
+
+def test_keduax(app):
+	print('\n ==============')
+	print('\n Runnning Second test')
+	home_screen.btn_explore.click()
+	home_screen.btn_home.click()
+	sleep(3)
+	print('Calling general method')
 	app.mondar_mandir()
