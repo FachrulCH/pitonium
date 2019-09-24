fa485f75bc7667e7473960081c4fa250919ad862
Fachrul Ch <fachrul.fch@gmail.com>
2019-09-24 17:13:31 +0700

    add allure decorator

--- tests/scenarios/explorer_test.py
+++ tests/scenarios/explorer_test.py
@@ -1,19 +1,23 @@
-from selene.conditions import visible
-from selene.support.conditions import have
+import datetime
+
+import allure
 from grappa import expect
+from selene.conditions import visible
 
-from testlib.ui.screens import home_screen
 from testlib.ui.screens import explorer_screen
+from testlib.ui.screens import home_screen
 from testlib.ui.screens import profile_screen
-from time import sleep
 
-
+@allure.title("This test has a custom title")
 def test_searching_person_and_follow(app):
-	home_screen.btn_explore.click()
-	explorer_screen.input_search.click()
-	explorer_screen.input_search.should_be(visible)
-	explorer_screen.input_search.send_keys('fachrulch')
-	print('Result should show')
+	allure.dynamic.description('Test search and follow :{}'.format(datetime.datetime.now()))
+	with allure.step("Given Instagram app is open"):
+		home_screen.btn_explore.click()
+	with allure.step("When I search for：{0}".format('FachrulCH')):
+		explorer_screen.input_search.click()
+		explorer_screen.input_search.should_be(visible)
+		explorer_screen.input_search.send_keys('fachrulch')
+		print('Result should show')
 	explorer_screen.list_result_person.first().click()
 	# breakpoint()
 	expect(profile_screen.text_profile_title.text).to.equal('fachrulch')
@@ -24,7 +28,12 @@ def test_searching_person_and_follow(app):
 		profile_screen.btn_follow.click()
 
 
+@allure.description("""
+Multiline test description.
+That comes from the allure.description decorator.
 
+Nothing special about it.
+""")
 def test_searching_hashtag(app):
 	home_screen.btn_explore.click()
 	explorer_screen.input_search.click()
--- tests/scenarios/instagram_test.py
+++ tests/scenarios/instagram_test.py
@@ -1,6 +1,9 @@
+import allure
+
 from testlib.ui.screens import home_screen
 from time import sleep
 
+@allure.step
 def test_pertamax(app):
 	print('Runnning first test')
 	home_screen.btn_explore.click()
@@ -9,6 +12,7 @@ def test_pertamax(app):
 	print('Calling general method')
 	app.mondar_mandir()
 
+@allure.step('This is description from allure step')
 def test_keduax(app):
 	print('\n ==============')
 	print('\n Runnning Second test')
